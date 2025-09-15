337de36a2ef771f6ac3774f7de586a81e8b90dde
106c13 <terbclub@gmail.com>
2025-09-15 13:20:31 +0400

    08 start

---
+++ cpp08/ex00/main.cpp
@@ -0,0 +1,44 @@
+#include "easyfind.hpp"
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    {
+        std::vector<int>    v;
+        v.push_back(10);
+        v.push_back(20);
+        v.push_back(30);
+
+        try
+        {
+            std::vector<int>::iterator it = easyfind(v, 20);
+            std::cout << "Found: " << *it << std::endl;
+            it = easyfind(v, 99);
+            std::cout << "Found: " << *it << std::endl;
+        }
+        catch (const std::exception& e)
+        {
+            std::cerr << e.what() << std::endl;
+        }
+    }
+    {
+        std::vector<int>    v;
+        v.push_back(10);
+        v.push_back(20);
+        v.push_back(30);
+
+        try
+        {
+            std::vector<int>::iterator it = easyfind(v, 0);
+            std::cout << "Found: " << *it << std::endl;
+            it = easyfind(v, 99);
+            std::cout << "Found: " << *it << std::endl;
+        }
+        catch (const std::exception& e)
+        {
+            std::cerr << e.what() << std::endl;
+        }
+    }
+}
+
